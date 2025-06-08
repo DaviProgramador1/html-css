853fae757caa3365f445d64f05eb94ebd148ff79
davi <zollyrparanaue@gmail.com>
2025-06-08 16:09:33 -0300

    aaaa

---
+++ exercicio/css22/java/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileBtn = document.getElementById("mobile_btn");
+  const mobileMenu = document.getElementById("mobile_menu");
+
+  mobileBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
